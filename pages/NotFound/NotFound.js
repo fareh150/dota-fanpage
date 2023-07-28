4d7710862b5284206f49f88ec7571664afe0672e
fareh romero vilchez <fareh.rv@gmail.com>
2023-07-28 10:52:04 +0200

    notFound page end

--- pages/NotFound/NotFound.js
+++ pages/NotFound/NotFound.js
@@ -2,7 +2,13 @@ import "./NotFound.css";
 
 const template = () => {
   return `
-      <h2>Aqui estoy NotFound</h2>
+      <div id="gamebackbox">
+        <h2 id="errorh2">Error 404 - Page Not Found</h2> 
+        <h3>¡¡¡¡Preguntar a Antonio!!!!</h3>
+        <iframe id="gif1" src="https://giphy.com/embed/QKUx6kHItu3ilaVMdn" width="480" height="338" frameBorder="0" ></iframe>
+        <iframe id="gif2" src="https://giphy.com/embed/ER2xgGF4SxOy512ewj" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+      </div>
+
       `;
 };
 
